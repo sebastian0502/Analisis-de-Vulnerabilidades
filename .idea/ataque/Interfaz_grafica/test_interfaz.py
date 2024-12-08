1c68dc56bef8746727bbfae4ffc329a61b1e69dc
Sebastian David Torres Reyes <142699563+sebastian0502@users.noreply.github.com>
2024-12-08 15:37:58 -0500

    Se sube test_interfaz.py

--- .idea/ataque/Interfaz_grafica/test_interfaz.py
+++ .idea/ataque/Interfaz_grafica/test_interfaz.py
@@ -17,10 +17,10 @@ class TestWiFiScanner(unittest.TestCase):
         passwords = scanner.leer_contrasenas_archivo("fake_path.txt")
         self.assertEqual(passwords, ["password1", "password2", "password3"])
 
-    @patch("Interfaz.PyWiFi")  # Simulamos el objeto PyWiFi
+    @patch("Interfaz.PyWiFi")  
     def test_scan_networks(self, mock_wifi):
         """Prueba del escaneo de redes WiFi"""
-        # Configuración del mock
+        
         mock_interface = MagicMock()
         mock_wifi.return_value.interfaces.return_value = [mock_interface]
         mock_interface.scan_results.return_value = [
@@ -29,10 +29,8 @@ class TestWiFiScanner(unittest.TestCase):
             MagicMock(ssid=""),
         ]
 
-        # Ejecutar la función
         redes = self.scanner.scan_networks()
 
-        # Se verifican los resultados
         self.assertIn("UNI_LIBRE_H", redes)
         self.assertIn("LIFIEE", redes)
         self.assertNotIn("", redes)
@@ -40,7 +38,7 @@ class TestWiFiScanner(unittest.TestCase):
     @patch("Interfaz.PyWiFi")
     def test_conectar_red_fallida(self, mock_wifi):
         """Prueba de conexión fallida a una red WiFi"""
-        # Configuración del mock
+        
         mock_interface = MagicMock()
         mock_wifi.return_value.interfaces.return_value = [mock_interface]
         mock_interface.status.return_value = const.IFACE_DISCONNECTED
@@ -53,31 +51,29 @@ class TestWiFiScanner(unittest.TestCase):
 class TestWifi(unittest.TestCase):
 
     def setUp(self):
-        # Crear una instancia de FuncionesPrograma para las pruebas
+        
         self.funciones = FuncionesPrograma()
             
     @patch("Interfaz.WiFiScanner.ataque_fuerza_bruta_wifi")
     def test_iniciar_ataque_red(self, mock_ataque):
         """Prueba del inicio de un ataque de fuerza bruta a una red"""
-        mock_ataque.return_value = None  #Se simula que no se encontro la contraseña
+        mock_ataque.return_value = None  
 
         funciones = FuncionesPrograma()
         funciones.redes_disponibles = ["RedPrueba"]
         funciones.opcion_red = MagicMock(get=MagicMock(return_value="RedPrueba"))
 
-        # Ejecutamos el método
         funciones.iniciar_ataque_red()
 
-        #Se verifica que el analisis inicio de manera correcta
         mock_ataque.assert_called_once_with("RedPrueba", 8, 10, unittest.mock.ANY)
 
 class TestFormulario(unittest.TestCase):
     def setUp(self):
-        #Instancia de funcionesprograma
+        
         self.funciones = FuncionesPrograma()
 
     def test_generate_passwords(self):
-        #Se verifica la generacion de contraseñas
+        
         passwords = list(self.funciones.generate_passwords(1, 1, None))
         expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -87,7 +83,7 @@ class TestFormulario(unittest.TestCase):
 
     @patch("Interfaz.requests.get")
     def test_fetch_login_token(self, mock_get):
-        # Simular una respuesta exitosa al obtener un token
+        
         mock_response = MagicMock()
         mock_response.text = '<input name="logintoken" value="fake_token">'
         mock_get.return_value = mock_response
@@ -101,31 +97,26 @@ class TestFormulario(unittest.TestCase):
     def test_brute_force_moodle(self, mock_get, mock_post, mock_fetch_token):
         mock_ventana_progreso = MagicMock()
 
-        # Se simula una respuesta exitosa de logintoken de la URL
         mock_response_get = MagicMock()
         mock_response_get.text = '<input name="logintoken" value="fake_token">'
         mock_get.return_value = mock_response_get
         
-        # Se simula la respuesta correcta del post
         mock_response_post = MagicMock()
         mock_response_post.text = "success"
         mock_post.return_value = mock_response_post
 
-        # Llamar a la función brute_force_moodle
         self.funciones.brute_force_moodle(
             "https://univirtual.uni.pe/login/index.php", 
             "user", 2, 2, ["password"], 
             mock_ventana_progreso
         )
         
-        # Se verifica que la ventana de progreso haya sido actualizada (simulacion)
         mock_ventana_progreso.actualizar_texto.assert_called()
 
-        # Asegurarse de que la solicitud POST incluya el logintoken simulado
         mock_post.assert_called_with(
             "https://univirtual.uni.pe/login/index.php",
             data={
-                "logintoken": "fake_token",  # Usamos el token simulado
+                "logintoken": "fake_token",  
                 "username": "user",
                 "password": "password"
             }
